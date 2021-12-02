4f3dfea99baba31bfab3d39578f9ab3ca4dbbdb6
Ihar Shauchuk <shauchuk.ihar@gmail.com>
2021-12-02 21:57:06 +0300

    added exercises for first homework

---
+++ hw_01/ex_01.py
@@ -0,0 +1,6 @@
+# Напишите программу, которая запрашивала бы у пользователя два числа
+# и выводила бы True или False в зависимости от того, больше первое число второго или нет.
+
+number_1 = input("Enter the first number: ")
+number_2 = input("Enter the second number: ")
+print(number_1 > number_2)
