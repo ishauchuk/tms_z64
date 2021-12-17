87ff661cbcc7e8ba6d27c2cc16b5f8fb273dc7d4
Ihar <shauchuk.ihar@gmail.com>
2021-12-17 18:42:40 +0300

    file names changed

--- hw_04/ex_04_01.py
+++ hw_04/ex_04_01.py
@@ -3,7 +3,8 @@ The following iteration sequence is defined for a set of numbers:
 If n is even n = n / 2. If n is odd n = n * 3 + 1.
 Using the above rule and starting at 13, we generate a sequence:
 13 -> 40 -> 20 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
-It can be seen that this sequence (from 13 to 1) contains 10 terms. Although this has not yet been proven (Collatz
+It can be seen that this sequence (from 13 to 1) contains 10 terms.
+Although this has not yet been proven (Collatz
 problem), it is believed that all start numbers end in 1.
 Which seed less than a million makes the longest chain?
 NOTE: Once the chain starts, the terms can chain one million.
