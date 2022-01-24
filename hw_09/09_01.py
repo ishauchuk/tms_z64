7903d9705a690a32b27ff164c8a5f40284097a30
Ihar Shauchuk <shauchuk.ihar@gmail.com>
2022-01-24 20:32:13 +0300

    added decorator versatility

--- hw_09/09_01.py
+++ hw_09/09_01.py
@@ -4,22 +4,22 @@ Write a decorator for a function that puts input data in reverse order.
 
 
 def decorator(func):
-    def wrapper(some_data):
-        data = some_data.split(' ')
-        data_1 = data[::-1]
-        data_2 = ' '.join(data_1)
-        return func(data_2)
+    def wrapper(*args, **kwargs):
+        data = []
+        data.extend(args)
+        data = data[::-1]
+        return func(*data)
 
     return wrapper
 
 
 if __name__ == "__main__":
-    user_input = input('Enter data separated by spaces:\n')
+    user_input = input('Enter data separated by spaces:\n').split(' ')
 
 
     @decorator
-    def changeling(user_input):
-        print(user_input)
+    def changeling(*args, **kwargs):
+        print(*args, **kwargs)
 
 
-    changeling(user_input)
+    changeling(*user_input)
