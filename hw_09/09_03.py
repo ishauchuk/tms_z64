c8e1ff525c218b52d65f7cba4139def729809183
Ihar Shauchuk <shauchuk.ihar@gmail.com>
2022-01-25 19:53:02 +0300

    added third task

---
+++ hw_09/09_03.py
@@ -0,0 +1,15 @@
+from memory_profiler import profile as check_RAM
+
+log = open("09_03_log.txt", "w+")
+
+
+@check_RAM(stream=log)
+def my_func():
+    a = [1] * (10 ** 6)
+    b = [2] * (2 * 10 ** 7)
+    del b
+    return a
+
+
+if __name__ == '__main__':
+    my_func()
