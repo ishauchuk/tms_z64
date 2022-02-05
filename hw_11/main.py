d1bcfa92deb1ca8a92eddc978eb9d0a0282d766f
Ihar Shauchuk <shauchuk.ihar@gmail.com>
2022-02-05 20:37:43 +0300

    added level setup

--- hw_11/main.py
+++ hw_11/main.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from settings import *
+from level import Level
 
 
 class Game:
@@ -11,6 +12,8 @@ class Game:
         pygame.display.set_caption('MyZelda')
         self.clock = pygame.time.Clock()
 
+        self.level = Level()
+
     def run(self):
         while True:
             for event in pygame.event.get():
@@ -19,6 +22,7 @@ class Game:
                     sys.exit()
 
             self.screen.fill('black')
+            self.level.run()
             pygame.display.update()
             self.clock.tick(FPS)
 
