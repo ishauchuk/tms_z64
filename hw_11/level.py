8ae85bfd8bbe9de8802b407c385c1300165ea6c5
Ihar Shauchuk <shauchuk.ihar@gmail.com>
2022-02-06 20:32:37 +0300

    added the overlap

--- hw_11/level.py
+++ hw_11/level.py
@@ -37,7 +37,6 @@ class Level:
 
 class YSortCameraGroup(pygame.sprite.Group):
     def __init__(self):
-
         # general setup
         super().__init__()
         self.display_surface = pygame.display.get_surface()
@@ -46,11 +45,12 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.offset = pygame.math.Vector2()
 
     def custom_draw(self, player):
-
         # getting the offset
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
 
-        for sprite in self.sprites():
+        # for sprite in self.sprites():
+        for sprite in sorted(self.sprites(),
+                             key=lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
             self.display_surface.blit(sprite.image, offset_pos)
