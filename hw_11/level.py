4a4651b9d61c9b19496c6e123749fce416529173
Ihar Shauchuk <shauchuk.ihar@gmail.com>
2022-02-11 22:31:37 +0300

    added ui for weapons

--- hw_11/level.py
+++ hw_11/level.py
@@ -6,6 +6,7 @@ from debug import debug
 from support import *
 from random import choice
 from weapon import Weapon
+from ui import UI
 
 
 class Level:
@@ -22,6 +23,9 @@ class Level:
 
         # attack sprites
         self.current_attack = None
+
+        # user interface
+        self.ui = UI()
 
     def create_map(self):
         layouts = {
@@ -69,6 +73,7 @@ class Level:
         # update and draw the game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
+        self.ui.display(self.player)
 
 
 class YSortCameraGroup(pygame.sprite.Group):
