01f5087f7aa2eabd07c6416e426ef03f4a1c72b5
Ihar Shauchuk <shauchuk.ihar@gmail.com>
2022-02-15 13:50:45 +0300

    added player and enemy iteractions

--- hw_11/level.py
+++ hw_11/level.py
@@ -19,11 +19,13 @@ class Level:
         self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
 
-        # sprite setup
-        self.create_map()
-
         # attack sprites
         self.current_attack = None
+        self.attack_sprites = pygame.sprite.Group()
+        self.attackable_sprites = pygame.sprite.Group()
+
+        # sprite setup
+        self.create_map()
 
         # user interface
         self.ui = UI()
@@ -51,7 +53,8 @@ class Level:
                         if style == 'grass':
                             random_grass_image = choice(graphics['grass'])
                             Tile((x, y),
-                                 [self.visible_sprites, self.obstacle_sprites],
+                                 [self.visible_sprites, self.obstacle_sprites,
+                                  self.attackable_sprites],
                                  'grass', random_grass_image)
                         if style == 'object':
                             surf = graphics['objects'][int(col)]
@@ -77,11 +80,13 @@ class Level:
                                 else:
                                     monster_name = 'squid'
                                 Enemy(monster_name, (x, y),
-                                      [self.visible_sprites],
-                                      self.obstacle_sprites)
+                                      [self.visible_sprites,
+                                       self.attackable_sprites],
+                                      self.obstacle_sprites, self.damage_player)
 
     def create_attack(self):
-        self.current_attack = Weapon(self.player, [self.visible_sprites])
+        self.current_attack = Weapon(self.player, [self.visible_sprites,
+                                                   self.attack_sprites])
 
     def create_magic(self, style, strength, cost):
         print(style)
@@ -93,11 +98,33 @@ class Level:
             self.current_attack.kill()
         self.current_attack = None
 
+    def player_attack_logic(self):
+        if self.attack_sprites:
+            for attack_sprite in self.attack_sprites:
+                collision_sprites = pygame.sprite.spritecollide(attack_sprite,
+                                                                self.attackable_sprites,
+                                                                False)
+                if collision_sprites:
+                    for target_sprite in collision_sprites:
+                        if target_sprite.sprite_type == 'grass':
+                            target_sprite.kill()
+                        else:
+                            target_sprite.get_damage(self.player,
+                                                     attack_sprite.sprite_type)
+
+    def damage_player(self, amount, attack_type):
+        if self.player.vulnerable:
+            self.player.health -= amount
+            self.player.vulnerable = False
+            self.player.hurt_time = pygame.time.get_ticks()
+            # spawn particles
+
     def run(self):
         # update and draw the game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
         self.visible_sprites.enemy_update(self.player)
+        self.player_attack_logic()
         self.ui.display(self.player)
 
 
--- hw_11/weapon.py
+++ hw_11/weapon.py
@@ -4,6 +4,7 @@ import pygame
 class Weapon(pygame.sprite.Sprite):
     def __init__(self, player, groups):
         super().__init__(groups)
+        self.sprite_type = 'weapon'
         direction = player.status.split('_')[0]
 
         # graphic
@@ -12,10 +13,14 @@ class Weapon(pygame.sprite.Sprite):
 
         # placement
         if direction == 'right':
-            self.rect = self.image.get_rect(midleft=player.rect.midright + pygame.math.Vector2(0, 16))
+            self.rect = self.image.get_rect(
+                midleft=player.rect.midright + pygame.math.Vector2(0, 16))
         elif direction == 'left':
-            self.rect = self.image.get_rect(midright=player.rect.midleft + pygame.math.Vector2(0, 16))
+            self.rect = self.image.get_rect(
+                midright=player.rect.midleft + pygame.math.Vector2(0, 16))
         elif direction == 'down':
-            self.rect = self.image.get_rect(midtop=player.rect.midbottom + pygame.math.Vector2(-10, 0))
+            self.rect = self.image.get_rect(
+                midtop=player.rect.midbottom + pygame.math.Vector2(-10, 0))
         else:
-            self.rect = self.image.get_rect(midbottom=player.rect.midtop + pygame.math.Vector2(-10, 0))
+            self.rect = self.image.get_rect(
+                midbottom=player.rect.midtop + pygame.math.Vector2(-10, 0))
