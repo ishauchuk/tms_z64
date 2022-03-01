82836c0a3c6896181eba0819d2f284498f09fc47
Ihar Shauchuk <shauchuk.ihar@gmail.com>
2022-03-01 16:52:53 +0300

    added validator and admin panel setup

--- hw_14/village_hall/warehouse/admin.py
+++ hw_14/village_hall/warehouse/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Wh
+from .models import Product
 
 
-@admin.register(Wh)
-class WhAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'amount', 'price', 'delivery_date', 'storage_time')
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'amount', 'price', 'delivery_date', 'storage_time']
+    list_editable = ['amount', 'price']
+    list_filter = ['price', 'amount']
+    ordering = ['-delivery_date']
+    list_per_page = 3
--- hw_14/village_hall/warehouse/models.py
+++ hw_14/village_hall/warehouse/models.py
@@ -1,19 +1,35 @@
 from django.db import models
+from django.core.validators import ValidationError
+from datetime import date
 
 
-class Wh(models.Model):
-    title = models.CharField(max_length=200, help_text="article name")
+def validate_storage_time(storage_time):
+    today = date.today()
+    if today > storage_time:
+        raise ValidationError("The expiration date can`t be in the past!")
+
+
+class Product(models.Model):
+
+    CURRENCY_CHOICES = [
+        ('BYN', 'Rubles'),
+        ('USD', 'Dollars'),
+        ('EUR', 'Euro'),
+    ]
+
+    title = models.CharField(max_length=200, help_text="article name", )
     amount = models.IntegerField(help_text="number of items")
     delivery_date = models.DateTimeField(auto_now_add=True)
-    storage_time = models.DateField()
-    price = models.DecimalField(max_digits=8, decimal_places=2)
+    storage_time = models.DateField(validators=[validate_storage_time])
+    price = models.DecimalField(max_digits=8, decimal_places=2, max_length=3, choices=CURRENCY_CHOICES, default='BYN')
 
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = 'Warehouse'
-        verbose_name_plural = 'Warehouses'
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
 
 
 
