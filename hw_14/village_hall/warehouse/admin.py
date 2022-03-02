d86326ec7f70356cfea3ea5c9365c3331822c4c1
Ihar Shauchuk <shauchuk.ihar@gmail.com>
2022-03-02 15:51:37 +0300

    added new columns

--- hw_14/village_hall/warehouse/admin.py
+++ hw_14/village_hall/warehouse/admin.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
 from .models import Product
-
+from django.db.models import QuerySet
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'amount', 'price', 'delivery_date', 'storage_time']
-    list_editable = ['amount', 'price']
+    # exclude = ['currency']
+    readonly_fields = ['currency']
+    list_display = ['title', 'amount', 'price', 'currency', 'currency_amount', 'currency_vat','delivery_date', 'storage_time', 'currency_discount']
+    list_editable = ['amount', 'price', 'currency']
     list_filter = ['price', 'amount']
     ordering = ['-delivery_date']
     list_per_page = 3
+    actions = ['set_to_dollars', 'set_to_euro', 'set_to_byn']
+
+    @admin.action(description='Set the dollars')
+    def set_to_dollars(self, request, QuerySet):
+        QuerySet.update(currency=Product.USD)
+
+    @admin.action(description='Set the euro')
+    def set_to_euro(self, request, QuerySet):
+        QuerySet.update(currency=Product.EUR)
+
+    @admin.action(description='Set the byn')
+    def set_to_byn(self, request, QuerySet):
+        QuerySet.update(currency=Product.BYN)
+
+
--- hw_14/village_hall/warehouse/models.py
+++ hw_14/village_hall/warehouse/models.py
@@ -3,32 +3,59 @@ from django.core.validators import ValidationError
 from datetime import date
 
 
+today = date.today()
+
+
 def validate_storage_time(storage_time):
-    today = date.today()
+    # today = date.today()
     if today > storage_time:
         raise ValidationError("The expiration date can`t be in the past!")
 
 
 class Product(models.Model):
-
+    BYN = 'BYN'
+    USD = 'USD'
+    EUR = 'EUR'
     CURRENCY_CHOICES = [
-        ('BYN', 'Rubles'),
-        ('USD', 'Dollars'),
-        ('EUR', 'Euro'),
+        (BYN, 'Rubles'),
+        (USD, 'Dollars'),
+        (EUR, 'Euro'),
     ]
 
     title = models.CharField(max_length=200, help_text="article name", )
     amount = models.IntegerField(help_text="number of items")
     delivery_date = models.DateTimeField(auto_now_add=True)
     storage_time = models.DateField(validators=[validate_storage_time])
-    price = models.DecimalField(max_digits=8, decimal_places=2, max_length=3, choices=CURRENCY_CHOICES, default='BYN')
+    price = models.FloatField(help_text="price in BYN")
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default='BYN')
 
     def __str__(self):
         return self.title
 
+    def currency_amount(self):
+        if self.currency == self.USD:
+            return round(self.price / 3, 2)
+        elif self.currency == self.EUR:
+            return round(self.price / 3.5, 2)
+        else:
+            return self.price
+    currency_amount.short_description = 'Price in currency'
+
+    def currency_vat(self):
+        return round(self.currency_amount() * 1.2, 2)
+    currency_vat.short_description = 'Price with vat'
+
+    def currency_discount(self):
+        if int(str(self.storage_time - today).split()[0]) < 10:
+            return round(self.currency_amount() * 0.7, 2)
+        else:
+            return 'no discount'
+    currency_discount.short_description = 'Price with discount'
+
     class Meta:
         verbose_name = 'Product'
         verbose_name_plural = 'Products'
+
 
 
 
